53ccbc17ff0c9cb5fa4b7bbaf59ab4a30aa60a91
Danny Fenstermaker <dannyfenstermaker@gmail.com>
2016-07-24 15:26:57 -0600

    webpack outputting correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,24 @@
 module.exports = {
+
+    entry: {
+        index: ['./src/index.js']
+    },
+
+    output: {
+        path: __dirname + "/dist",
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                loader: "babel",
+                 query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
 }
